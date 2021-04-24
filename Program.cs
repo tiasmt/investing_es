994870acc73dde5fbac03a3fa59d9a48f1bba03b
Tias Fenech <tiasfenech@gmail.com>
2021-04-24 22:56:46 +0200

    Cleaning display

--- Program.cs
+++ Program.cs
@@ -20,12 +20,12 @@ namespace investing_es
             var key = string.Empty;
             while (key != "X")
             {
-                System.Console.WriteLine("D: Deposit");
-                System.Console.WriteLine("W: Withdraw");
-                System.Console.WriteLine("B: Buy Stock");
-                System.Console.WriteLine("S: Sell Stock");
-                System.Console.WriteLine("P: Portfolio");
-                System.Console.WriteLine("E: Events");
+                PrintMenuItem("Deposit", ConsoleColor.DarkRed);
+                PrintMenuItem("Withdraw", ConsoleColor.DarkCyan);
+                PrintMenuItem("Buy Stock", ConsoleColor.DarkGray);
+                PrintMenuItem("Sell Stock", ConsoleColor.DarkGreen);
+                PrintMenuItem("Portfolio", ConsoleColor.DarkYellow);
+                PrintMenuItem("Events", ConsoleColor.Yellow);
                 System.Console.WriteLine(">  ");
                 key = Console.ReadLine()?.ToUpperInvariant();
                 System.Console.WriteLine();
@@ -46,12 +46,12 @@ namespace investing_es
                         System.Console.WriteLine($"{username} Withdrew: {moneyToWithdraw}");
                         break;
                     case "P":
-                        System.Console.WriteLine($"{username} Money: {portfolio.GetState().Money}");
+                        System.Console.WriteLine($"Money: {portfolio.GetState().Money:0.##}");
                         if(portfolio.GetState().Shares != null) 
                         {
                             foreach(var stockOwned in portfolio.GetState().Shares)
                             {
-                                System.Console.WriteLine($"{username} Stock Ticker: {stockOwned.Key} Quantity: {stockOwned.Value.NumberOfShares} Average Price: {stockOwned.Value.Price} Profit: {portfolio.GetState().Profit}");
+                                System.Console.WriteLine($"Stock Ticker: {stockOwned.Key} Quantity: {stockOwned.Value.NumberOfShares} Average Price: {stockOwned.Value.Price:0.##} Profit: {portfolio.GetState().Profit:0.##}");
                             }
                         }
                         break;
@@ -60,14 +60,14 @@ namespace investing_es
                         var stock = GetStock();
                         var price = GetPrice();
                         portfolio.BuyShares(stock, amountOfSharesToBuy, price);
-                        System.Console.WriteLine($"{username} Bought: {stock} Amount: {amountOfSharesToBuy} Price: {price}");
+                        System.Console.WriteLine($"{username} Bought: {stock} Amount: {amountOfSharesToBuy} Price: {price:0.##}");
                         break;
                     case "S":
                         var amountOfSharesToSell = GetAmount();
                         stock = GetStock();
                         price = GetPrice();
                         portfolio.SellShares(stock, amountOfSharesToSell, price);
-                        System.Console.WriteLine($"{username} Sold: {stock} Amount: {amountOfSharesToSell} Price: {price}");
+                        System.Console.WriteLine($"{username} Sold: {stock} Amount: {amountOfSharesToSell} Price: {price:0.##}");
                         break;
                     case "E":
                     //todo - fix how events are retrieved
@@ -96,25 +96,43 @@ namespace investing_es
 
         private static string GetUsername()
         {
-            System.Console.WriteLine("Username: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("Username: ");
+            Console.ForegroundColor = ConsoleColor.White;
             return Console.ReadLine()?.ToUpperInvariant();
         }
         private static int GetAmount()
         {
-            System.Console.WriteLine("Amount: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("Amount: ");
+            Console.ForegroundColor = ConsoleColor.White;
             return Convert.ToInt32(Console.ReadLine()?.ToUpperInvariant());
         }
 
         private static string GetStock()
         {
-            System.Console.WriteLine("Stock Ticker: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("Stock Ticker: ");
+            Console.ForegroundColor = ConsoleColor.White;
             return Console.ReadLine()?.ToUpperInvariant();
         }
 
         private static double GetPrice()
         {
-            System.Console.WriteLine("Price: ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            System.Console.Write("Price: ");
+            Console.ForegroundColor = ConsoleColor.White;
             return double.Parse(Console.ReadLine()?.ToUpperInvariant());
+        }
+
+        private static void PrintMenuItem(string item, ConsoleColor color = ConsoleColor.White)
+        {
+            var itemShort = item.FirstOrDefault();
+            Console.ForegroundColor = color;
+            System.Console.Write($"{itemShort}: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.Write($"{item}");
+            System.Console.WriteLine();
         }
     }
 }
